52cb2abffa368e25299d23b855851ef78e177acf
zeong <zhelingzelin@gmail.com>
2022-10-31 00:41:02 +0800

    [billsplit] update with repeatable person forms

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,31 @@
-var newPersonHtml = "<div>" +
-"<input type=\"text\" name=\"personName\" placeholder=\"name\" size=\"20\" checked>" +
-"<input type=\"text\" name=\"item\" placeholder=\"item\" size=\"20\" checked>" +
-"</div>"
+
+
+var sessionData = {};
+
+var personFormsHtml;
+// var xhr= new XMLHttpRequest();
+// xhr.open('GET', 'personForms.html', true);
+// xhr.onreadystatechange= function() {
+//     if (this.readyState!==4) return;
+//     if (this.status!==200) return; // or whatever error handling you want
+//     personFormsHtml = this.responseText;
+// };
+// xhr.send();
+// $("#main_block").load("personForms.html");
+fetch("personForms.html").then(response => {
+    // var result = response.text();
+    // return result;
+    return response.text();
+}).then(text => personFormsHtml = text)
 ;
+
 
 function addNewPerson() {
     var mainBlockElem = document.getElementById("main_block");
-    mainBlockElem.innerHTML += newPersonHtml;
+    // mainBlockElem.appendChild();
+    // $("#main_block").load("personForms.html");
+    // $(document).l
+    mainBlockElem.innerHTML += personFormsHtml;
 }
 
 // var but;
@@ -14,8 +33,27 @@ function addFixedItem(elem) {
     // console.log(elem);
     var parent = elem.closest('form');
     var itemData = parent.getElementsByClassName('item-data')[0];
-    var itemName = parent.getElementsByClassName('input-name')[0];
-    var itemPrice = parent.getElementsByClassName('input-price')[0];
+    var itemName = parent.getElementsByClassName('item-name')[0];
+    var itemPrice = parent.getElementsByClassName('item-price')[0];
     itemData.innerHTML += itemName.value + " --> "+ itemPrice.value +"<br/>";
+}
+
+
+function clearData() {
+    var itemNameList = document.getElementsByClassName('item-name');
+    var itemNamePrice = document.getElementsByClassName('item-price');
+    for(let i = 0; i < itemNameList.length; ++i) {
+        itemNameList[i].value = "";
+        itemNamePrice[i].value = "";
+    }
+}
+
+
+function loadData() {
+
+}
+
+function saveData(dataName) {
+    
 }
 
